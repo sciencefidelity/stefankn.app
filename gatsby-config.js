94759a8cb2aaaf5f353d598c7bc469d3909771d1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-16 21:13:50 +0000

    Add sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/knapp-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/knapp-icon.png`,
       },
     },
     {
